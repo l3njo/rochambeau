aa5711c42a15f7df7df1669fd128480c7422f320
l3njo <plenjo15@gmail.com>
2019-07-22 18:07:48 +0300

    Added webhook for heroku deployment

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func init() {
 }
 
 func main() {
-	bot = tbot.New(token)
+	bot = tbot.New(token, tbot.WithWebhook("https://rochambeau-bot.herokuapp.com", ":"+os.Getenv("PORT")))
 	app.client = bot.Client()
 	bot.HandleMessage("/start", app.startHandler)
 	bot.HandleMessage("/play", app.playHandler)
